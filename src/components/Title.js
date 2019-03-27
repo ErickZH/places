70cf4078106d658ceffcb9e803e13550be4ff863
ErickZH <zaraterick@outlook.com>
2019-03-26 22:47:21 -0600

    My first component.

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class Title extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>Places</h1>
+          <p>Los mejores lugares para visitar.</p>
+        </div>
+      )
+    }
+}
